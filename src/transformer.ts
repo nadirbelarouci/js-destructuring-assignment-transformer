6868d7120790562411736e0031e360619259f9bf
Nadir Belarouci <nadir.belarrouci@gmail.com>
2019-07-24 21:49:15 +0200

    creating basic visitor

---
+++ src/transformer.ts
@@ -0,0 +1,31 @@
+import ts = require("typescript");
+
+const source = `
+  const two = 2;
+  const four = 4;
+`;
+
+function numberTransformer<T extends ts.Node>(): ts.TransformerFactory<T> {
+    return context => {
+        const visit: ts.Visitor = node => {
+            if (ts.isNumericLiteral(node)) {
+                return ts.createStringLiteral(node.text);
+            }
+            return ts.visitEachChild(node, child => visit(child), context);
+        };
+
+        return node => ts.visitNode(node, visit);
+    };
+}
+
+let result = ts.transpileModule(source, {
+    compilerOptions: { module: ts.ModuleKind.CommonJS },
+    transformers: { before: [numberTransformer()] }
+});
+
+console.log(result.outputText);
+
+/*
+  var two = "2";
+  var four = "4";
+*/
