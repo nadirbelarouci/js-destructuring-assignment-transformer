c7333e1f19a9af9f845f72806e98d9ef89ad125a
Nadir Belarouci <nadir.belarrouci@gmail.com>
2019-07-24 21:54:06 +0200

    detecting all ElementAccessExpression and store their variable declarations Note that Each Block has his own Declarations i.e ArrayBindingPatterns

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,5 +1,22 @@
 import ts = require("typescript");
 
+const source = `
+  function ping(arr){
+      var b = arr[1];
+      var c = arr[5],p = da,m = arr[9],p =arr[3];
+      var d = zz[0];
+      var x = zz[1];
+      if(true){
+        var d = ddd[0];
+        var x = ddd[1];
+      }
+      
+        var e = gg[0];
+        var f = gg[1];
+      const two = 2;
+      const four = 4;
+  }
+`;
 
 class ArrayBindingPatternInstance {
     bingingElements: Map<number, ts.BindingElement> = new Map();
@@ -24,27 +41,72 @@ class ArrayBindingPatternInstance {
 
 }
 
-function numberTransformer<T extends ts.Node>(): ts.TransformerFactory<T> {
-    return context => {
-        const visit: ts.Visitor = node => {
-            if (ts.isNumericLiteral(node)) {
-                return ts.createStringLiteral(node.text);
-            }
-            return ts.visitEachChild(node, child => visit(child), context);
-        };
+class ArrayBindingPatternDeclarations {
+    arrayBindingPatterns: Map<string, ArrayBindingPatternInstance> = new Map();
 
-        return node => ts.visitNode(node, visit);
-    };
+    add(variableDeclaration: ts.VariableDeclaration) {
+        const initializer = variableDeclaration.initializer as ts.ElementAccessExpression;
+        const array = initializer.expression.getText();
+
+        if (this.arrayBindingPatterns.has(array)) {
+            const arrayBindingElement = this.arrayBindingPatterns.get(array) as ArrayBindingPatternInstance;
+             arrayBindingElement.addBindingElement(variableDeclaration);
+
+
+        } else {
+
+            this.arrayBindingPatterns.set(array, new ArrayBindingPatternInstance(variableDeclaration))
+        }
+    }
+
+
 }
 
+class ArrayBindingPatternTransformer {
+    declarations: Map<ts.Block, ArrayBindingPatternDeclarations> = new Map();
+
+
+    initArrayBindingPatternDeclarations<T extends ts.Node>(): ts.TransformerFactory<T> {
+
+        const stack: ts.Block[] = [];
+
+        return context => {
+            const visit: ts.Visitor = node => {
+                if (ts.isBlock(node)) {
+                    stack.push(node);
+                    this.declarations.set(node, new ArrayBindingPatternDeclarations());
+                }
+                if (ts.isElementAccessExpression(node)) {
+                    const currentBlock: ts.Block = stack[stack.length - 1];
+                    const arrayBindingPatternDeclarations = this.declarations.get(currentBlock) as ArrayBindingPatternDeclarations;
+                    arrayBindingPatternDeclarations.add(node.parent as ts.VariableDeclaration);
+
+                }
+                const children = ts.visitEachChild(node, child => visit(child), context);
+                if (node === stack[stack.length - 1]) {
+                    stack.pop();
+                }
+                return children;
+            };
+
+            return node => ts.visitNode(node, visit);
+        };
+    }
+
+
+}
+
+
+const arrayBindingPatternTransformer = new ArrayBindingPatternTransformer();
+
 let result = ts.transpileModule(source, {
-    compilerOptions: { module: ts.ModuleKind.CommonJS },
-    transformers: { before: [numberTransformer()] }
+    compilerOptions: {module: ts.ModuleKind.CommonJS, target: ts.ScriptTarget.ES2016},
+    transformers: {
+        before: [
+            arrayBindingPatternTransformer.initArrayBindingPatternDeclarations(),
+        ]
+    }
 });
 
-console.log(result.outputText);
 
-/*
-  var two = "2";
-  var four = "4";
-*/
+console.log(result.outputText);
